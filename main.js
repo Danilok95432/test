afe4ed9ae8507fd5276b595adcce55ca0dd1e0c4
Danila <danila.dvoretskov@yandex.ru>
2024-02-04 16:22:37 +0300

    forth_commit

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-    autoplay: {
-        delay: 5000,
-    },
+    
   
     // If we need pagination
     pagination: {
@@ -42,6 +40,58 @@ let connect_btn = document.getElementById('connect').addEventListener('click', (
     modal.style.display = 'flex'
 })
 
-let close_btn = document.querySelector('.close-btn').addEventListener('click', () => {
-    modal.style.display = 'none'
+let close_btn = document.querySelectorAll('.close-btn')
+let close_btn_modal = document.getElementById('close_btn')
+
+for(let i = 0; i < close_btn.length; i++)
+  close_btn[i].addEventListener('click', () => {
+      modal.style.display = 'none'
+  })
+
+close_btn_modal.addEventListener('click', () => {
+  modal.style.display = 'none'
+})
+
+let submit_btn = document.getElementById("submit_btn")
+let nameModal = document.getElementById("name")
+let phone = document.getElementById("phone_form")
+
+const phoneMask = new IMask(phone, {
+  mask: "+{7}(000)000-00-00",
+});
+
+nameModal.addEventListener('change', () => {
+  if(nameModal.value != '' && phoneMask.masked.isComplete){
+    submit_btn.classList.add('active')
+    submit_btn.style.pointerEvents = 'all'
+  } else {
+    submit_btn.classList.remove('active')
+    submit_btn.style.pointerEvents = 'none'
+  }
+})
+
+phone.addEventListener("input", () => {
+  if (phoneMask.masked.isComplete && nameModal.value != '') {
+    submit_btn.classList.add("active");
+    submit_btn.style.pointerEvents = 'all'
+  } else {
+    submit_btn.classList.remove("active");
+    submit_btn.style.pointerEvents = 'none'
+  }
+});
+
+
+let modalCongrat = document.querySelector('.modal-congratulation')
+let modalWindow = document.querySelector('.modal-window') 
+
+submit_btn.addEventListener('click', async () => {
+  modalWindow.style.display = 'none'
+  modalCongrat.style.display = 'flex'
+  return await new Promise(resolve => {
+    const data = {
+      name: nameModal.value,
+      phone: phone.value
+    }
+    resolve(data)
+  })
 })
